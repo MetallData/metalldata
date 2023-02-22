db96320f9ccaf384b88c65fe85761717fff6a52c
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2023-02-22 11:06:22 -0800

    (mjl-count) add optional argument to count all
    the argument count_all can be set to true, in which case
    the specified selection criteria is ignored.

--- src/MetallJsonLines/mjl-count.cpp
+++ src/MetallJsonLines/mjl-count.cpp
@@ -16,6 +16,9 @@ namespace xpr     = experimental;
 namespace
 {
 const std::string methodName = "count";
+
+const std::string COUNT_ALL_NAME = "count_all";
+const std::string COUNT_ALL_DESC = "if true, the selection criteria is ignored";
 } // anonymous
 
 
@@ -29,21 +32,21 @@ int ygm_main(ygm::comm& world, int argc, char** argv)
   clip.member_of(CLASS_NAME, "A " + CLASS_NAME + " class");
   clip.add_required_state<std::string>(ST_METALL_LOCATION, "Metall storage location");
 
+  clip.add_optional<bool>(COUNT_ALL_NAME, COUNT_ALL_DESC, false);
+
   if (clip.parse(argc, argv, world)) { return 0; }
 
   try
   {
     const std::string    dataLocation = clip.get_state<std::string>(ST_METALL_LOCATION);
+    const bool           countAll     = clip.get<bool>(COUNT_ALL_NAME);
     xpr::MetallJsonLines lines{world, metall::open_read_only, dataLocation, MPI_COMM_WORLD};
-    const std::size_t    res          = lines.filter(filter(world.rank(), clip))
-                                             .count();
+    const std::size_t    res          = countAll ? lines.count()
+                                                 : lines.filter(filter(world.rank(), clip)).count();
 
     if (world.rank() == 0)
     {
-      std::stringstream msg;
-
-      msg << "Selected " << res << " rows." << std::flush;
-      clip.to_return(msg.str());
+      clip.to_return(res);
     }
   }
   catch (const std::exception& err)
