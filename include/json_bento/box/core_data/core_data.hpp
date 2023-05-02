2e3a2b95df341b7ea2afea2c7e51884c165bd118
Keita Iwabuchi <iwabuchi1@llnl.gov>
2023-05-01 19:59:41 -0700

    (JSON Bento) bugfix: string_view related error when Boost < 1.79 is used

--- include/json_bento/box/core_data/core_data.hpp
+++ include/json_bento/box/core_data/core_data.hpp
@@ -1,5 +1,5 @@
-// Copyright 2023 Lawrence Livermore National Security, LLC and other MetallData Project Developers.
-// See the top-level COPYRIGHT file for details.
+// Copyright 2023 Lawrence Livermore National Security, LLC and other MetallData
+// Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: MIT
 
@@ -11,18 +11,17 @@
 #include <metall/container/string.hpp>
 #include <metall/container/vector.hpp>
 
+#include <json_bento/box/core_data/value_locator.hpp>
 #include <json_bento/details/compact_adjacency_list.hpp>
 #include <json_bento/details/compact_string_storage.hpp>
 #include <json_bento/details/data_storage.hpp>
 #include <json_bento/details/key_store.hpp>
 #include <json_bento/details/key_value_pair.hpp>
-#include <json_bento/box/core_data/value_locator.hpp>
 
 namespace json_bento::jbdtl {
 
-template <typename Alloc>
-struct core_data {
- public:
+template <typename Alloc> struct core_data {
+public:
   using allocator_type = Alloc;
   using string_type = metall::container::basic_string<
       char, std::char_traits<char>,
@@ -46,11 +45,8 @@ struct core_data {
   core_data() = default;
 
   explicit core_data(allocator_type alloc)
-      : string_storage(alloc),
-        root_value_storage(alloc),
-        array_storage(alloc),
-        object_storage(alloc),
-        key_storage(alloc) {}
+      : string_storage(alloc), root_value_storage(alloc), array_storage(alloc),
+        object_storage(alloc), key_storage(alloc) {}
 
   ~core_data() noexcept = default;
 
@@ -61,14 +57,14 @@ struct core_data {
   key_storage_type key_storage{allocator_type{}};
 };
 
-}  // namespace json_bento::jbdtl
+} // namespace json_bento::jbdtl
 
 // TODO: make a better implementation
 namespace json_bento::jbdtl {
 
 template <typename value_type, typename core_data_type>
-inline void add_value(const value_type& value, core_data_type& core_data,
-                      value_locator& loc) {
+inline void add_value(const value_type &value, core_data_type &core_data,
+                      value_locator &loc) {
   if (value.is_null()) {
     loc.reset();
   } else if (value.is_bool()) {
@@ -84,7 +80,7 @@ inline void add_value(const value_type& value, core_data_type& core_data,
         value.as_string().c_str(), value.as_string().size());
   } else if (value.is_array()) {
     const auto row = core_data.array_storage.push_back();
-    for (const auto& v : value.as_array()) {
+    for (const auto &v : value.as_array()) {
       core_data.array_storage.push_back(row, value_locator());
       const auto col = core_data.array_storage.size(row) - 1;
       add_value(v, core_data, core_data.array_storage.at(row, col));
@@ -92,12 +88,17 @@ inline void add_value(const value_type& value, core_data_type& core_data,
     loc.emplace_array_index() = row;
   } else if (value.is_object()) {
     const auto row = core_data.object_storage.push_back();
-    for (const auto& kv : value.as_object()) {
+    for (const auto &kv : value.as_object()) {
+#if BOOST_VERSION >= 107900
       const auto key_loc = core_data.key_storage.find_or_add(kv.key());
-      core_data.object_storage.push_back(row,
-                                   key_value_pair(key_loc, value_locator()));
+#else
+      const auto key_loc = core_data.key_storage.find_or_add(kv.key().data());
+#endif
+      core_data.object_storage.push_back(
+          row, key_value_pair(key_loc, value_locator()));
       const auto col = core_data.object_storage.size(row) - 1;
-      add_value(kv.value(), core_data, core_data.object_storage.at(row, col).value());
+      add_value(kv.value(), core_data,
+                core_data.object_storage.at(row, col).value());
     }
     loc.emplace_object_index() = row;
   } else {
@@ -105,14 +106,20 @@ inline void add_value(const value_type& value, core_data_type& core_data,
   }
 }
 
+/// \brief Add a value at the end of a core data as a root value.
+/// \tparam value_type JSON value type that is compatible with Boost/Metall JSON
+/// value.
+/// \tparam core_data_type Core data type.
+/// \param source_value A value to be added.
+/// \param core_data A core data to which the value is added.
+/// \return The index of the added value.
 template <typename value_type, typename core_data_type>
-inline auto push_back_root_value(const value_type& source_value,
-                                 core_data_type& core_data) {
+inline auto push_back_root_value(const value_type &source_value,
+                                 core_data_type &core_data) {
   const auto idx = core_data.root_value_storage.size();
   core_data.root_value_storage.emplace_back();
   add_value(source_value, core_data, core_data.root_value_storage.back());
   return idx;
 }
 
-}  // namespace json_bento::jbdtl
-
+} // namespace json_bento::jbdtl
