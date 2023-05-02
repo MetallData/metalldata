4a6ea934dc4ecbcd8002640dd6fdf196e46ab7ef
Keita Iwabuchi <iwabuchi1@llnl.gov>
2023-05-01 19:59:41 -0700

    (JSON Bento) bugfix: string_view related error when Boost < 1.79 is used

--- include/json_bento/value_to.hpp
+++ include/json_bento/value_to.hpp
@@ -1,5 +1,5 @@
-// Copyright 2023 Lawrence Livermore National Security, LLC and other MetallData Project Developers.
-// See the top-level COPYRIGHT file for details.
+// Copyright 2023 Lawrence Livermore National Security, LLC and other MetallData
+// Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: MIT
 
@@ -12,13 +12,13 @@ namespace json_bento {
 
 namespace {
 namespace bj = boost::json;
-}  // namespace
+} // namespace
 
 namespace jbdtl {
 
 template <typename allocator_type>
-inline void value_to_helper(const value_accessor<allocator_type>& jv,
-                            bj::value& out_bj_value) {
+inline void value_to_helper(const value_accessor<allocator_type> &jv,
+                            bj::value &out_bj_value) {
   if (jv.is_bool()) {
     out_bj_value = jv.as_bool();
   } else if (jv.is_int64()) {
@@ -31,16 +31,20 @@ inline void value_to_helper(const value_accessor<allocator_type>& jv,
     out_bj_value = jv.as_string().c_str();
   } else if (jv.is_array()) {
     bj::array bj_array;
-    const auto& arr = jv.as_array();
+    const auto &arr = jv.as_array();
     for (std::size_t i = 0; i < arr.size(); ++i) {
       bj_array.template emplace_back(value_to(arr[i]));
     }
     out_bj_value = bj_array;
   } else if (jv.is_object()) {
     bj::object bj_object;
-    const auto& obj = jv.as_object();
-    for (auto kv : obj) {
+    const auto obj = jv.as_object();
+    for (const auto &kv : obj) {
+#if BOOST_VERSION >= 107900
       bj_object[kv.key()] = value_to(kv.value());
+#else
+      bj_object[kv.key().data()] = value_to(kv.value());
+#endif
     }
     out_bj_value = bj_object;
   } else if (jv.is_null()) {
@@ -49,18 +53,18 @@ inline void value_to_helper(const value_accessor<allocator_type>& jv,
 }
 
 template <typename allocator_type>
-inline boost::json::value value_to(const value_accessor<allocator_type>& jv) {
+inline boost::json::value value_to(const value_accessor<allocator_type> &jv) {
   bj::value out_value;
   value_to_helper(jv, out_value);
   return out_value;
 }
 
-}  // namespace jbdtl
-}  // namespace json_bento
+} // namespace jbdtl
+} // namespace json_bento
 
 namespace json_bento {
 template <typename T, typename allocator_type>
-inline T value_to(const jbdtl::value_accessor<allocator_type>& jv) {
+inline T value_to(const jbdtl::value_accessor<allocator_type> &jv) {
   return jbdtl::value_to(jv);
 }
-}  // namespace json_bento
+} // namespace json_bento
