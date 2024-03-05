731a4111f793e7bb9004e948198dd677b2238d69
Keita Iwabuchi <iwabuchi1@llnl.gov>
2024-03-05 12:28:16 -0800

    Do not append auto-vertices with column names

--- sample/MetallGraphParquet.py
+++ sample/MetallGraphParquet.py
@@ -7,7 +7,7 @@ from clippy import clippy_import, config
 # Change the following lines to your environment
 metall_dir = './metalldata-parquet-graph'
 this_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.abspath(this_dir + '../')
+src_dir = os.path.abspath(this_dir + '/../')
 build_dir = src_dir + '/build/'
 data_dir = src_dir + '/sample/data/'
 
@@ -15,18 +15,23 @@ clippy_import(build_dir + '/src/MetallGraph')
 
 config.cmd_prefix = f'mpirun -np 2'
 # Or
-# config.cmd_prefix = f'srun -N 1 --ntasks-per-node 2 --mpibind=off'
+#config.cmd_prefix = f'srun -N 1 --ntasks-per-node 2 --mpibind=off'
 
-graph = MetallGraph(metall_dir, key="id", srckey="src", dstkey="dst", overwrite=True)
+graph = MetallGraph(metall_dir, key="id", srckey="Src", dstkey="Dst", overwrite=True)
+graph.read_edges(data_dir + "/edges.parquet", fileType='parquet', autoVertices=["src", "dst"])
+# If there is a vertex file:
+# graph = MetallGraph(metall_dir, key="id", srckey="src", dstkey="dst", overwrite=True)
+# graph.read_vertices(data_dir + "/vertices.parquet", fileType='parquet')
+# graph.read_edges(data_dir + "/edges.parquet", fileType='parquet')
 
-graph.read_vertices(data_dir + "/vertices.parquet", fileType='parquet')
+print("Graph: {}".format(graph.count()))
+# Graph: {'nodes': 2603, 'edges': 16358}
 
-graph.read_edges(data_dir + "/edges.parquet", fileType='parquet')
+print("#of CCs: {}".format(graph.connected_components()))
+# #of CCs: 6
 
-graph.count()
+print("kcore sizess (k=[0, 3]): {}".format(graph.kcore(3)))
+# kcore sizess (k=[0, 3]): [2603, 1959, 1590, 1307]
 
-graph.connected_components()
-
-graph.kcore(3)
-
-graph[graph.nodes.kcore >= 3].connected_components()
+print("#of CCs on kcore graph (k=3): {}".format(graph[graph.nodes.kcore >= 3].connected_components()))
+# CCs on kcore graph (k=3): 259
