de91855d3a7d97f897ea14908c759b69b8ac7ea9
Keita Iwabuchi <iwabuchi1@lln.gov>
2025-03-12 14:22:28 -0700

    (MS) Brush up on the series container demo

--- examples/multiseries/demo_series_container.cpp
+++ examples/multiseries/demo_series_container.cpp
@@ -61,7 +61,7 @@ void run_bench(const std::filesystem::path &metall_path,
 
   std::cout << "Total #of records: " << record_store->num_records() <<
     std::endl;
-  std::cout << "String store size: " << string_store->size() << std::endl;
+  std::cout << "#of unique strings: " << string_store->size() << std::endl;
   std::cout << get_dir_usage(metall_path) << std::endl;
 }
 
@@ -69,7 +69,7 @@ int main(int argc, char **argv) {
   std::filesystem::path metall_path{"./metall_data"};
   size_t num_records = 1'000'000;
   parse_option(argc, argv, metall_path, num_records);
-  
+
   std::cout << "Ingest int64_t values" << std::endl;
   std::cout << "Dense container" << std::endl;
   run_bench<int64_t>(metall_path,
