c5e55409013b3a63030b4126a38fee2c90753207
Seth Bromberger <github@bromberger.com>
2026-01-21 23:33:17 +0000

    stderr example in ingest_parquet_edges - requires ygm 0.9-dev

--- src/clippy/MetallGraph/utils.hpp
+++ src/clippy/MetallGraph/utils.hpp
@@ -1,61 +1,18 @@
 #pragma once
 #include <metalldata/metall_graph.hpp>
-#include <boost/json.hpp>
-#include <expected>
-#include <unordered_set>
 
 namespace metalldata {
+std::expected<metall_graph::series_name, metall_graph::return_code> obj2sn(
+  const boost::json::object &obj);
 
-inline std::expected<metall_graph::series_name, metall_graph::return_code>
-obj2sn(const boost::json::object &obj) {
-  metall_graph::return_code to_return;
-  if (!obj.contains("rule")) {
-    to_return.error = "Series name invalid (norule)";
-    return std::unexpected(to_return);
-  }
+std::expected<std::unordered_set<metall_graph::series_name>,
+              metall_graph::return_code>
+obj2sn(const std::unordered_set<boost::json::object> &objset);
 
-  auto rule     = obj.at("rule");
-  auto rule_obj = rule.get_object();
-  if (!rule_obj.contains("var")) {
-    to_return.error = "Series name invalid (novar)";
-    return std::unexpected(to_return);
-  }
-  return metall_graph::series_name(rule_obj.at("var").as_string());
-}
+std::expected<std::vector<metall_graph::series_name>, metall_graph::return_code>
+obj2sn(const std::vector<boost::json::object> &objset);
 
-inline std::expected<std::unordered_set<metall_graph::series_name>,
-                     metall_graph::return_code>
-obj2sn(const std::unordered_set<boost::json::object> &objset) {
-  metall_graph::return_code to_return;
+ygm::log_level loglevel_py2ygm(
+  int pyloglevel, ygm::log_level default_level = ygm::log_level::warn);
 
-  std::unordered_set<metall_graph::series_name> sns;
-  for (const auto &obj : objset) {
-    auto r = obj2sn(obj);
-
-    if (!r.has_value()) {
-      return std::unexpected(r.error());
-    }
-    sns.insert(metall_graph::series_name(r.value()));
-  }
-
-  return sns;
-}
-
-inline std::expected<std::vector<metall_graph::series_name>,
-                     metall_graph::return_code>
-obj2sn(const std::vector<boost::json::object> &objset) {
-  metall_graph::return_code to_return;
-
-  std::vector<metall_graph::series_name> sns;
-  for (const auto &obj : objset) {
-    auto r = obj2sn(obj);
-
-    if (!r.has_value()) {
-      return std::unexpected(r.error());
-    }
-    sns.push_back(metall_graph::series_name(r.value()));
-  }
-
-  return sns;
-}
 }  // namespace metalldata
