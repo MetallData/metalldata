1c709ebf3be019f3d788acaf8e88129f918d85bf
Seth Bromberger <sbromberger@users.noreply.github.com>
2026-03-02 18:42:49 -0800

    indegree and outdegree with tests (#98)

--- test/clippy/test_mg.py
+++ test/clippy/test_mg.py
@@ -327,5 +327,53 @@ def test_mg_rename_series(metallgraph):
         metallgraph.rename_series(metallgraph.node.nsamp1, "node.nsamp1")
 
 
-    
+@pytest.mark.order(12)
+def test_mg_indegree(metallgraph):
+    degs1 = {
+        "5clique-a": {"indeg1": 0},
+        "5clique-b": {"indeg1": 1},
+        "5clique-c": {"indeg1": 2},
+        "5clique-d": {"indeg1": 3},
+        "5clique-e": {"indeg1": 4}
+    }
 
+    degs2 = {
+        "5clique-d": {"indeg1": 3},
+        "5clique-e": {"indeg1": 4}
+    }
+
+
+    metallgraph.in_degree("indeg1")
+    select_data = metallgraph.select_nodes()
+    is_specific(select_data, "id", degs1)
+
+    metallgraph.in_degree("indeg2", where = metallgraph.node.indeg1 > 2)
+    select_data = metallgraph.select_nodes()
+    is_specific(select_data, "id", degs2)
+
+
+@pytest.mark.order(12)
+def test_mg_outdegree(metallgraph):
+    degs1 = {
+        "5clique-a": {"outdeg1": 4},
+        "5clique-b": {"outdeg1": 3},
+        "5clique-c": {"outdeg1": 2},
+        "5clique-d": {"outdeg1": 1},
+        "5clique-e": {"outdeg1": 0}
+    }
+
+    degs2 = {
+        "5clique-a": {"outdeg1": 4},
+        "5clique-b": {"outdeg1": 3}
+    }
+
+
+    metallgraph.out_degree("outdeg1")
+    select_data = metallgraph.select_nodes()
+    is_specific(select_data, "id", degs1)
+
+    metallgraph.out_degree("outdeg2", where = metallgraph.node.outdeg1 > 2)
+    select_data = metallgraph.select_nodes()
+    is_specific(select_data, "id", degs2)
+
+# {'id': '5clique-a', 'indeg1': 0, 'randpct': 4.519041400698163, 'uuid': 'bffdb73b-e003-4904-b4c4-fcfb6de44ded'}, {'id': '5clique-b', 'indeg1': 1, 'nsamp1': True, 'randpct': 18.729939416659157, ...}, {'id': '5clique-c', 'indeg1': 2, 'randpct': 57.53399788992132, 'uuid': 'bffdb73b-e003-4904-b4c4-fcfb6de44ded'}, {'id': '5clique-d', 'indeg1': 3, 'randpct': 3.4017999660346714, 'uuid': 'bffdb73b-e003-4904-b4c4-fcfb6de44ded'}
