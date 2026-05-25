b329cfd51d454cfc85b6820ea0373d59cb6e26ff
Seth Bromberger <github@bromberger.com>
2026-05-25 18:35:11 +0000

    individual tests

---
+++ test/clippy/tests/test_mg_ingest.py
@@ -0,0 +1,15 @@
+import pytest
+from clippy import MetallGraph  # type: ignore
+from conftest import DATA_DIR, is_as_described, is_as_selected
+
+
+@pytest.fixture()
+def empty_graph(tmp_path):
+    return MetallGraph(str(tmp_path / "metallgraph.db"))
+
+
+def test_mg_ingest_parquet(empty_graph):
+    empty_graph.ingest_parquet_edges(DATA_DIR + "/test", "s", "t")
+    is_as_described(empty_graph, 21, 28)
+    el = empty_graph.select_edges()
+    is_as_selected(el, {}, ["u", "v", "graphnum", "relevant"], ["foo", "field_does_not_exist"])
---
+++ test/clippy/tests/test_mg_topk.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+@pytest.fixture()
+def topk_graph(metallgraph):
+    metallgraph.add_faker("node.randpct", "percentage")
+    return metallgraph
+
+
+def test_mg_topk(topk_graph):
+    mg = topk_graph
+
+    results = mg.topk(mg.edge.randint, k=5, addl_series=[mg.edge.name])
+    assert len(results) == 5
+    curr_max = 10000
+    for result in results:
+        assert len(result) == 2
+        assert result[0] <= curr_max
+        curr_max = result[0]
+
+    results = mg.topk(mg.node.randpct, k=50000)
+    assert len(results) == mg.describe()["nv"]
+    curr_max = 10000
+    for result in results:
+        assert len(result) == 1
+        assert result[0] <= curr_max
+        curr_max = result[0]
+
+    results = mg.topk(mg.edge.randint, where=mg.edge.graphnum == 1)
+    rresults = [x[0] for x in results]
+    assert rresults == [92, 84, 76, 57, 33, 19]
