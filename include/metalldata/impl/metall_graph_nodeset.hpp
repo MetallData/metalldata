db585e094706290608d70b901a6c565599e2d1b9
Roger Pearce <rpearce@llnl.gov>
2026-06-13 04:34:48 +0000

    nodeset inherits from ygm::container::set<node_locator>

--- include/metalldata/impl/metall_graph_nodeset.hpp
+++ include/metalldata/impl/metall_graph_nodeset.hpp
@@ -2,22 +2,18 @@
 #include <ygm/container/set.hpp>
 
 namespace metalldata {
-class metall_graph::nodeset {
+/**
+ * @brief nodeset is specialized for node_locators by deriving from
+ * ygm::container::set Provides for_all_local that paritions locators by their
+ * owner before executing user Fn.
+ *
+ */
+class metall_graph::nodeset : public ygm::container::set<node_locator> {
+ private:
+  using base = ygm::container::set<node_locator>;
+
  public:
-  template <typename T>
-  nodeset(T&& arg) : m_set(std::forward<T>(arg)) {}
-
-  /**
-   * @brief Inserts node_locator into set
-   *
-   * @param nl
-   */
-  void async_insert(node_locator nl) { m_set.async_insert(nl); }
-
-  template <typename Fn>
-  void for_all(Fn func) {
-    m_set.for_all(func);
-  }
+  using base::base;
 
   /**
    * @brief Collective for_all that executes func over only local nodes
@@ -31,30 +27,17 @@ class metall_graph::nodeset {
     static Fn* spfunc = nullptr;
     spfunc = &func;
     static ygm::comm* spcomm = nullptr;
-    spcomm = &(m_set.comm());
-    m_set.comm().cf_barrier();
-    m_set.for_all([](node_locator nl) {
+    spcomm = &(comm());
+    comm().cf_barrier();
+    for_all([](node_locator nl) {
       int  dest = owner(nl);
       auto rlambda = [](local_node_idx_type nid) { (*spfunc)(nid); };
       spcomm->async(dest, rlambda, local(nl));
     });
-    m_set.comm().barrier();
+    comm().barrier();
     spfunc = nullptr;
     spcomm = nullptr;
   }
-
-  /**
-   * @brief Collective operation to look up items that exist within set
-   *
-   * @param values Values local rank wants to look up in set
-   * @return `std::set` of provided values that exist within the YGM set
-   */
-  template <typename STLValueContainer>
-  std::set<node_locator> gather_values(const STLValueContainer& values) {
-    return m_set.gather_values(values);
-  }
-
- private:
-  ygm::container::set<node_locator> m_set;
 };
+
 }  // namespace metalldata
