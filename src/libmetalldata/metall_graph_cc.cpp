5a55f759c448ce7916e29022b904d2a889362044
Roger Pearce <rpearce@llnl.gov>
2026-07-22 16:46:47 +0000

    fixes opt

--- src/libmetalldata/metall_graph_cc.cpp
+++ src/libmetalldata/metall_graph_cc.cpp
@@ -78,15 +78,6 @@ result<> metall_graph::connected_components(const series_name&  out_name,
       where);
   }
 
-  // adj_list.for_all([&](const std::string&                                v,
-  //                      std::pair<std::string, std::vector<std::string>>& adj)
-  //                      {
-  //   adj.first = v;
-  //   for (const auto& n : adj.second) {
-  //     adj.first = std::min(adj.first, n);
-  //   }
-  // });
-
   static auto* sp_adj_list = &adj_list;
   m_comm.barrier();
 
@@ -94,7 +85,11 @@ result<> metall_graph::connected_components(const series_name&  out_name,
     void operator()(const std::string&                                v,
                     std::pair<std::string, std::vector<std::string>>& adj,
                     const std::string&                                cc_id) {
-      if (cc_id < adj.first) {
+      auto min_id = adj.first;
+      for (const auto& n : adj.second) {
+        min_id = std::min(min_id, n);
+      }
+      if (adj.first == min_id) {
         adj.first = cc_id;
         for (const auto& n : adj.second) {
           sp_adj_list->async_visit(n, cc_visitor{}, cc_id);
