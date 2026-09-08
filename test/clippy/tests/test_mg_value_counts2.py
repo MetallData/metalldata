555a3eafad012ca9430d856f6b8f31f4a8f184da
Seth Bromberger <github@bromberger.com>
2026-09-08 21:36:07 +0000

    simplify dict creation

--- test/clippy/tests/test_mg_value_counts2.py
+++ test/clippy/tests/test_mg_value_counts2.py
@@ -8,7 +8,7 @@ def test_mg_value_counts2_edge(metallgraph):
     # edge.graphnum value_counts is {2: 10, 1: 6, 3: 6, 0: 6}, so the count
     # "10" occurs once and the count "6" occurs three times.
     results = metallgraph.value_counts2(metallgraph.edge.graphnum)
-    counts = {r[0]: r[1] for r in results}
+    counts = dict(results)
     assert counts[10] == 1
     assert counts[6] == 3
 
@@ -17,7 +17,7 @@ def test_mg_value_counts2_where(metallgraph):
     results = metallgraph.value_counts2(
         metallgraph.edge.graphnum, where=metallgraph.edge.graphnum == 0
     )
-    counts = {r[0]: r[1] for r in results}
+    counts = dict(results)
     assert len(counts) == 1
     assert counts[6] == 1
 
@@ -26,7 +26,7 @@ def test_mg_value_counts2_bool(metallgraph):
     # edge.relevant value_counts is {True: 12, False: 16}, two distinct
     # counts, each occurring once.
     results = metallgraph.value_counts2(metallgraph.edge.relevant)
-    counts = {r[0]: r[1] for r in results}
+    counts = dict(results)
     assert len(counts) == 2
     assert counts[12] == 1
     assert counts[16] == 1
@@ -36,7 +36,7 @@ def test_mg_value_counts2_node(metallgraph):
     # node.gnum value_counts is {3: 7, None: 14}, two distinct counts, each
     # occurring once.
     results = metallgraph.value_counts2(metallgraph.node.gnum)
-    counts = {r[0]: r[1] for r in results}
+    counts = dict(results)
     assert len(counts) == 2
     assert counts[7] == 1
     assert counts[14] == 1
