d314d9c6ccf12de850a68eb99036a8bf0077c700
Seth Bromberger <sbromberger@users.noreply.github.com>
2026-09-10 08:01:09 -0700

    Sbromberger/improve degree (#189)
    * added pasync_set_node_column_by_locator. Modified CC to use it. Modified private degree method to use it.
    * updated degrees() to use new pasync_set_node_column_by_locator

--- src/libmetalldata/metall_graph_degree.cpp
+++ src/libmetalldata/metall_graph_degree.cpp
@@ -6,6 +6,7 @@
 // TODO: we could probably implement this with a counting set instead
 // of a map.
 
+#include <memory>
 #include <string>
 #include <utility>
 #include <variant>
@@ -58,15 +59,6 @@ result<> metall_graph::priv_in_out_degree(
   series_name name, const metall_graph::where_clause& where, bool outdeg) {
   using record_id_type = record_store_type::record_id_type;
 
-  edge_series_idx_type degcol, otherdegcol;
-  if (outdeg) {
-    degcol = m_u_col_idx;
-    otherdegcol = m_v_col_idx;
-  } else {
-    degcol = m_v_col_idx;
-    otherdegcol = m_u_col_idx;
-  }
-
   if (!name.is_node_series()) {
     return std::unexpected(
       std::format("invalid series name: {}", name.qualified()));
@@ -78,14 +70,15 @@ result<> metall_graph::priv_in_out_degree(
   }
 
   auto                                      edges_ = m_pedges;
-  ygm::container::map<std::string, int64_t> degrees(m_comm);
+  ygm::container::map<node_locator, int64_t> degrees(m_comm);
+  // static auto*                               sp_degrees = &degrees;
 
-  std::vector<std::string> nodes;
   priv_for_all_nodes(
     [&](local_node_idx_type nid) {
       std::string_view node_name = pl_get_node_label(nid);
+      auto             nloc = make_node_locator(m_comm.rank(), nid);
 
-      degrees.async_insert(std::string(node_name), 0);
+      degrees.async_insert(nloc, 0);
     },
     where);
 
@@ -95,19 +88,15 @@ result<> metall_graph::priv_in_out_degree(
     [&](local_edge_idx_type eid) {
       // Note: clangd may report a false positive error on the next line
       // The code compiles and runs correctly
-      auto edge_name_o = pl_get_edge_field<std::string_view>(degcol, eid);
-      YGM_ASSERT_RELEASE(edge_name_o.has_value());
-      std::string_view edge_name = edge_name_o.value();
-      degrees.async_visit(std::string(edge_name),
-                          [](const auto& key, auto& val) { val++; });
+      auto [u, v] = pl_get_edge_uv_locators(eid);
+      if (!outdeg) {
+        std::swap(u, v);
+      }
+      degrees.async_visit(u, [](const auto& key, auto& val) { val++; });
       // for undirected edges, add the reverse.
       bool is_directed = pl_edge_is_directed(eid);
       if (!is_directed) {
-        auto reverseedge_name_o =
-          pl_get_edge_field<std::string_view>(otherdegcol, eid);
-        YGM_ASSERT_RELEASE(reverseedge_name_o.has_value());
-        degrees.async_visit(std::string(reverseedge_name_o.value()),
-                            [](const auto& key, auto& val) { val++; });
+        degrees.async_visit(v, [](const auto& key, auto& val) { val++; });
       }
     },
     where);
@@ -121,8 +110,7 @@ result<> metall_graph::priv_in_out_degree(
   //   degrees.async_insert_or_assign(node_name, deg_ct);
   // }
 
-  m_comm.barrier();
-  return priv_set_node_series(name, degrees);
+  return pasync_set_node_column_by_locator(name, degrees);
 }
 
 result<> metall_graph::degrees(series_name in_name, series_name out_name,
@@ -148,14 +136,14 @@ result<> metall_graph::degrees(series_name in_name, series_name out_name,
       std::format("series {} already exists", out_name.qualified()));
   }
 
-  ygm::container::map<std::string, int64_t> indegrees(m_comm);
-  ygm::container::map<std::string, int64_t> outdegrees(m_comm);
+  ygm::container::map<node_locator, int64_t> indegrees(m_comm);
+  ygm::container::map<node_locator, int64_t> outdegrees(m_comm);
 
   priv_for_all_nodes(
-    [&](local_node_idx_type nid) {
-      auto node_name = pl_get_node_label(nid);
-      indegrees.async_insert(std::string(node_name), 0);
-      outdegrees.async_insert(std::string(node_name), 0);
+    [&](local_node_idx_type nid_l) {
+      auto nid = make_node_locator(m_comm.rank(), nid_l);
+      indegrees.async_insert(nid, 0);
+      outdegrees.async_insert(nid, 0);
     },
     where);
 
@@ -165,22 +153,16 @@ result<> metall_graph::degrees(series_name in_name, series_name out_name,
     [&](local_edge_idx_type eid) {
       // Note: clangd may report a false positive error on the next line
       // The code compiles and runs correctly
-      auto [u, v] = pl_get_edge_uv_labels(eid);
-      std::string in_edge_name(u);
-      std::string out_edge_name(v);
-      indegrees.async_visit(in_edge_name,
-                            [&](const auto& key, auto& val) { val++; });
+      auto [u, v] = pl_get_edge_uv_locators(eid);
+      indegrees.async_visit(u, [&](const auto& key, auto& val) { val++; });
 
-      outdegrees.async_visit(out_edge_name,
-                             [&](const auto& key, auto& val) { val++; });
+      outdegrees.async_visit(v, [&](const auto& key, auto& val) { val++; });
 
       bool is_directed = pl_edge_is_directed(eid);
       if (!is_directed) {
-        indegrees.async_visit(out_edge_name,
-                              [&](const auto& key, auto& val) { val++; });
+        indegrees.async_visit(u, [&](const auto& key, auto& val) { val++; });
 
-        outdegrees.async_visit(in_edge_name,
-                               [&](const auto& key, auto& val) { val++; });
+        outdegrees.async_visit(v, [&](const auto& key, auto& val) { val++; });
       }
     },
     where);
@@ -189,93 +171,80 @@ result<> metall_graph::degrees(series_name in_name, series_name out_name,
   // with a barrier. But that's spooky action at a distance, so we will be
   // explicit here.
   m_comm.barrier();
-
-  // TODO: we want to abstract this to priv_set_node_series because this is a
-  // common operation. Make this a private function inside metall_graph.
-
-  // create a node_local map of record id to node value.
-  std::map<std::string, local_node_idx_type> node_to_id{};
-  priv_for_all_nodes([&](local_node_idx_type nid) {
-    std::string_view node = pl_get_node_label(nid);
-    node_to_id[std::string(node)] = nid;
-  });
-
-  // create series and store index so we don't have to keep looking it up.
-  auto in_deg_idx = m_pnodes->add_series<int64_t>(in_name.unqualified());
-  auto out_deg_idx = m_pnodes->add_series<int64_t>(out_name.unqualified());
 
   // add the values to the degrees series. We are taking advantage of the fact
   // that the node information is local from the degrees shared counting set
   // because it uses the same partitioning scheme as we used when we added the
   // nodes in ingest.
 
-  auto to_return = priv_set_node_series(in_name, indegrees);
-  auto to_return2 = priv_set_node_series(out_name, outdegrees);
+  auto to_return = pasync_set_node_column_by_locator(in_name, indegrees);
+  auto to_return2 = pasync_set_node_column_by_locator(out_name, outdegrees);
   to_return.merge_warnings(to_return2);
 
   return to_return;
 }
 
-result<> metall_graph::degrees2(series_name in_name, series_name out_name,
-                                const metall_graph::where_clause& where) {
-  using record_id_type = record_store_type::record_id_type;
+// TODO: Decide whether we want to use counting sets for this.
+// result<> metall_graph::degrees2(series_name in_name, series_name out_name,
+//                                 const metall_graph::where_clause& where) {
+//   using record_id_type = record_store_type::record_id_type;
 
-  if (!in_name.is_node_series()) {
-    return std::unexpected(
-      std::format("invalid series name: {}", in_name.qualified()));
-  }
+//   if (!in_name.is_node_series()) {
+//     return std::unexpected(
+//       std::format("invalid series name: {}", in_name.qualified()));
+//   }
 
-  if (!out_name.is_node_series()) {
-    return std::unexpected(
-      std::format("invalid series name: {}", out_name.qualified()));
-  }
+//   if (!out_name.is_node_series()) {
+//     return std::unexpected(
+//       std::format("invalid series name: {}", out_name.qualified()));
+//   }
 
-  if (m_pnodes->contains_series(in_name.unqualified())) {
-    return std::unexpected(
-      std::format("series {} already exists", in_name.qualified()));
-  }
-  if (m_pnodes->contains_series(out_name.unqualified())) {
-    return std::unexpected(
-      std::format("series {} already exists", out_name.qualified()));
-  }
+//   if (m_pnodes->contains_series(in_name.unqualified())) {
+//     return std::unexpected(
+//       std::format("series {} already exists", in_name.qualified()));
+//   }
+//   if (m_pnodes->contains_series(out_name.unqualified())) {
+//     return std::unexpected(
+//       std::format("series {} already exists", out_name.qualified()));
+//   }
 
-  auto                                      edges_ = m_pedges;
-  ygm::container::counting_set<std::string> indegrees(m_comm);
-  ygm::container::counting_set<std::string> outdegrees(m_comm);
+//   auto                                      edges_ = m_pedges;
+//   ygm::container::counting_set<std::string> indegrees(m_comm);
+//   ygm::container::counting_set<std::string> outdegrees(m_comm);
 
-  priv_for_all_edges(
-    [&](local_edge_idx_type eid) {
-      auto [u, v] = pl_get_edge_uv_labels(eid);
-      std::string in_edge_name(u);
-      std::string out_edge_name(v);
-      indegrees.async_insert(in_edge_name);
-      outdegrees.async_insert(out_edge_name);
+//   priv_for_all_edges(
+//     [&](local_edge_idx_type eid) {
+//       auto [u, v] = pl_get_edge_uv_labels(eid);
+//       std::string in_edge_name(u);
+//       std::string out_edge_name(v);
+//       indegrees.async_insert(in_edge_name);
+//       outdegrees.async_insert(out_edge_name);
 
-      auto is_directed = pl_edge_is_directed(eid);
+//       auto is_directed = pl_edge_is_directed(eid);
 
-      if (!is_directed) {
-        indegrees.async_insert(out_edge_name);
-        outdegrees.async_insert(in_edge_name);
-      }
-    },
-    where);
+//       if (!is_directed) {
+//         indegrees.async_insert(out_edge_name);
+//         outdegrees.async_insert(in_edge_name);
+//       }
+//     },
+//     where);
 
-  // not strictly required because the subsequent loop over degrees begins
-  // with a barrier. But that's spooky action at a distance, so we will be
-  // explicit here.
-  m_comm.barrier();
+//   // not strictly required because the subsequent loop over degrees begins
+//   // with a barrier. But that's spooky action at a distance, so we will be
+//   // explicit here.
+//   m_comm.barrier();
 
-  std::map<std::string, int64_t> local_indeg_i64 = {indegrees.begin(),
-                                                    indegrees.end()};
-  std::map<std::string, int64_t> local_outdeg_i64 = {indegrees.begin(),
-                                                     indegrees.end()};
-  auto to_return = priv_set_node_series(in_name, local_indeg_i64);
+//   std::map<std::string, int64_t> local_indeg_i64 = {indegrees.begin(),
+//                                                     indegrees.end()};
+//   std::map<std::string, int64_t> local_outdeg_i64 = {indegrees.begin(),
+//                                                      indegrees.end()};
+//   auto to_return = priv_set_node_series(in_name, local_indeg_i64);
 
-  auto to_return2 = priv_set_node_series(out_name, local_outdeg_i64);
+//   auto to_return2 = priv_set_node_series(out_name, local_outdeg_i64);
 
-  to_return.merge_warnings(to_return2);
+//   to_return.merge_warnings(to_return2);
 
-  return to_return;
-}
+//   return to_return;
+// }
 
 }  // namespace metalldata
